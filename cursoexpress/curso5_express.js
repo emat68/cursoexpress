f77dbfe345fcd99a0d8e905e7b6b336eca73a82f
emat68 <emalvarezt@hotmail.com>
2019-10-08 10:12:07 -0300

    eliminar carpeta

--- cursoexpress/curso5_express.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-var express = require('express');
-let app = express()
-.use(function (req, res, next) {
-    res.end('Hola dos...');
-});
-http.createServer(app).listen(3000);
-
